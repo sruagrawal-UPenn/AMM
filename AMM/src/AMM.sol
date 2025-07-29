9857c825dd96f593c90a77e376bddf91ec3e9048
sruagrawal-UPenn <sru28@seas.upenn.edu>
2025-07-28 20:17:33 -0500

    Update AMM.sol

--- AMM/src/AMM.sol
+++ AMM/src/AMM.sol
@@ -85,9 +85,9 @@ contract AMM is AccessControl{
 	*/
 	function provideLiquidity( uint256 amtA, uint256 amtB ) public {
 		require( amtA > 0 || amtB > 0, 'Cannot provide 0 liquidity' );
-		ERC20(tokenA).transferFrom(msg.sender, address(this), qtyA);
-		ERC20(tokenB).transferFrom(msg.sender, address(this), qtyB);
-
+		ERC20(tokenA).transferFrom(msg.sender, address(this), amtA);
+		ERC20(tokenB).transferFrom(msg.sender, address(this), amtB);
+	
 		invariant = ERC20(tokenA).balanceOf(address(this)) * ERC20(tokenB).balanceOf(address(this));
 		emit LiquidityProvision( msg.sender, amtA, amtB );
 	}
